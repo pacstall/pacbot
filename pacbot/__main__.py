d184d9f8e8e1a54674b9ce361e923bea84638e89
Sourajyoti Basak <wiz28@protonmail.com>
2021-12-24 06:09:13 +0000

    docs: add license (#1)
    * docs: add license
    
    * docs(license): add license boilerplate
    
    * docs(main): add license boilerplate

--- pacbot/__main__.py
+++ pacbot/__main__.py
@@ -1,3 +1,26 @@
+#     ____             ____        __
+#    / __ \____ ______/ __ )____  / /_
+#   / /_/ / __ `/ ___/ __  / __ \/ __/
+#  / ____/ /_/ / /__/ /_/ / /_/ / /_
+# /_/    \__,_/\___/_____/\____/\__/
+#
+# Copyright (C) 2021-Present
+#
+# This file is part of PacBot.
+#
+# PacBot is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PacBot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PacBot.  If not, see <https://www.gnu.org/licenses/>.
+
 from os import getenv
 
 import nextcord
