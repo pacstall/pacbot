7dec0ab45a9ef8e31661556ec9483c6f2d4ee613
Sourajyoti Basak <wiz28@protonmail.com>
2022-11-05 03:07:47 +0530

    refactor(main): don't use dotenv_codegen

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![allow(clippy::wildcard_imports)]
 
-use dotenv_codegen::dotenv;
+use std::env;
+
+use dotenv::dotenv;
 use poise::serenity_prelude as serenity;
 use serenity::{GatewayIntents, GuildId};
 
@@ -9,7 +11,7 @@ mod commands;
 type Error = Box<dyn std::error::Error + Send + Sync>;
 type PoiseResult = Result<(), Error>;
 type Context<'a> = poise::Context<'a, Data, Error>;
-// User data, which is stored and accessible in all command invocations
+
 pub struct Data {}
 
 async fn on_ready(
@@ -17,10 +19,7 @@ async fn on_ready(
     _ready: &serenity::Ready,
     framework: &poise::Framework<Data, Error>,
 ) -> Result<Data, Error> {
-    let pacstall_guild_id: u64 = dotenv!("PACSTALL_GUILDID")
-        .to_string()
-        .parse::<u64>()
-        .unwrap();
+    let pacstall_guild_id: u64 = env::var("PACSTALL_GUILDID")?.parse::<u64>()?;
 
     let builder = poise::builtins::create_application_commands(&framework.options().commands);
 
@@ -39,6 +38,8 @@ async fn on_ready(
 
 #[tokio::main]
 async fn main() {
+    dotenv().expect("Unable to load .env!");
+
     tracing_subscriber::fmt()
         .with_max_level(tracing::Level::INFO)
         .init();
@@ -58,7 +59,7 @@ async fn main() {
             },
             ..Default::default()
         })
-        .token(dotenv!("DISCORD_TOKEN"))
+        .token(env::var("DISCORD_TOKEN").unwrap())
         .intents(GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT)
         .user_data_setup(|ctx, ready, framework| Box::pin(on_ready(ctx, ready, framework)));
 
